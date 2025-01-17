5e3aa4697efe8ce10960492d06c9a4278c105ba5
wzesk <walter@conformlab.com>
2025-01-17 09:43:25 -0500

    fixed model setup

--- predict.py
+++ predict.py
@@ -1,7 +1,8 @@
 # predict.py
 from cog import BasePredictor, Input, Path
 from PIL import Image
-import RealESRGAN.model as re
+from RealESRGAN import RealESRGAN
+import torch
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
@@ -15,10 +16,15 @@ class Predictor(BasePredictor):
         Superresolve a low resolution image and get a high resolution image.
         """
         # Load image
-    
         lr_img = Image.open(image).convert("RGB")
+
+        # setup RealESRGAN model
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model = RealESRGAN(device, scale=4)
+        model.load_weights('weights/RealESRGAN_x4.pth', download=True)
+
         # Run upsampling
-        hr_img = re.predict(lr_img)
+        hr_img = model.predict(lr_img)
         # Save the resulting mask
         output_path = "upsampled_image.png"
         hr_img.save(output_path)
